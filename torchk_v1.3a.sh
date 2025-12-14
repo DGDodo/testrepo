b63a05ffa59a13ce4bae982763b69cdb3b10d577
DGDodo <100221343+DGDodo@users.noreply.github.com>
2025-12-14 01:35:59 +0100

    testing red LED blink

--- torchk_v1.3a.sh
+++ torchk_v1.3a.sh
@@ -28,7 +28,7 @@
 
 # v1.4 (future)
 # - Use of function(s) for LED changes in the program
-#
+# - Add red blinking (info) LED for Tor not running
 
 # Used:
 # - /etc/tor/torchk.sh                  - Folder holds 'this script' and its output-file
@@ -38,47 +38,62 @@
 # TODO:
 # - Change hourly crontab check to 5 or 10 minutes if failed?
 
+# Parameters
+
 # Program version
 Pversion=1.3
-
 # Set log parameter
 OUTPUT=/var/log/tor/torchk.log
-
 # Set output file parameter
 filestr1=/tmp/torchk.html
-
 # Remove output file if exist
 if [ -f $filestr1 ]; then rm $filestr1; fi
-
 # Get program ID
 progid=$$
-
 # Get curl version
 vCurl=$(opkg list-installed | grep 'curl - ' | cut -c8-)
 if [ ${#vCurl} -eq 0 ]; then progid=0; fi
-
 # We now use board_name instead of model, due to capitals in string 'model'.
 # There is difference in OpenWrt versions (P2812) and device model name.
 DEVICE=$(ubus call system board | grep board_name | cut -f4 -d\")
-
 # Get lan ip
 lanip=$(uci show | grep lan.ipaddr | cut -d\' -f2)
+
+# Functions
 
 # Function LEDs (on=error or off=OK)
 AdjustLEDs() {
 if [ "$1" = "on" ]; then 
   LEDon="none";
   LEDoff="default-on";
-else
+fi
+if [ "$1" = "off" ]; then
   LEDon="default-on";
   LEDoff="none";
 fi
-if [ $DEVICE = "avm,fritzbox-4040" ]; then echo $LEDoff > /sys/class/leds/red:info/trigger; fi
+if [ "$1" = "blink" ]; then
+  LEDon="none"
+  LEDoff="timer"
+  LEDdelay=250
+fi
+if [ $DEVICE = "avm,fritzbox-4040" ]; then 
+  echo $LEDoff > /sys/class/leds/red:info/trigger;
+  if [ LEDoff = "timer" ]; then
+    echo $LEDdelay > /sys/class/leds/red:info/delay_on;
+    echo $LEDdelay > /sys/class/leds/red:info/delay_off;
+  fi
+fi
 if [ $DEVICE = "linksys,whw03v2" ]; then
   echo $LEDoff > /sys/class/leds/red:indicator/trigger;
   echo $LEDon > /sys/class/leds/green:indicator/trigger;
+  if [ LEDoff = "timer" ]; then
+    echo $LEDdelay > /sys/class/leds/red:indicator/delay_on;
+    echo $LEDdelay > /sys/class/leds/red:indicator/delay_off;
+  fi
 fi
 }
+
+# Program
 
 # Screen header
 echo ""
@@ -122,7 +137,7 @@ else
   if [ $progid -eq 0 ]; then echo "Package 'curl' is not installed."; fi
   if [ ! "$(service tor status)" = "running" ]; then 
     echo "Service 'tor' is not running.";
-    AdjustLEDs "on"
+    AdjustLEDs "blink"
   fi
 fi
 
